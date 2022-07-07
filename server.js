d59bd13ed526c623d4cb5efb6844d427f48f7838
KingMo <macv123321@gmail.com>
2022-07-06 23:05:48 -0400

    added to server.js

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Moyman321',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Express middleware
 app.get('/', (req, res) => {
